f1eef891545b0831e46331b5dda1387bc0acff6a
Kristian Gonzalez <gjosue521@gmail.com>
2024-11-01 22:14:04 -0600

    :bug: Solucion de bugs 2

--- VeterinariaFronted/obj/Debug/net8.0/VeterinariaFronted.AssemblyInfo.cs
+++ VeterinariaFronted/obj/Debug/net8.0/VeterinariaFronted.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VeterinariaFronted")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92ba4984639c9195858521e19b80cfe034b6af8")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeterinariaFronted")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeterinariaFronted")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
